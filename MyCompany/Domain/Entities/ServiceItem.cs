5a2af05a462f14b3948615ff83ab5e63eab0209f
vlad kryglikov <vladkryglikov@mail.ru>
2022-03-25 16:25:44 +0300

    Добавьте файлы проекта.

---
+++ MyCompany/Domain/Entities/ServiceItem.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyCompany.Domain.Entities
+{
+    public class ServiceItem : EntityBase
+    {
+        [Required(ErrorMessage = "Заполните название услуги")]
+        [Display(Name = "Новости")]
+        public override string Title { get; set; }
+
+        [Display(Name = "Краткое описание")]
+        public override string Subtitle { get; set; }
+
+        [Display(Name = "Полное описание")]
+        public override string Text { get; set; }
+    }
+}
